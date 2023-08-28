db714de5124a899f23a2d57ecb12a300db82db7b
HoangNgocThang <thanghai.tb123@gmail.com>
2023-08-28 11:22:11 +0700

    cm

---
+++ src/controllers/users.js
@@ -0,0 +1,8 @@
+
+const getUser = (req, res, next)=> {
+    res.send('helo')
+}
+
+module.exports = {
+    getUser
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const {getUser} = require('../controllers/users');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', getUser);
 
 module.exports = router;
