a83bc538b3a10b4f4dc8bd9c5bc4c7a93dabef0b
HoangNgocThang <thanghai.tb123@gmail.com>
2023-09-18 14:44:44 +0700

    cm

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+// const { Sequelize } = require('sequelize');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -7,5 +8,10 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_DATABASE
   })
+
+// const connection =  new Sequelize( process.env.DB_NAME_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
 
 module.exports = connection;
